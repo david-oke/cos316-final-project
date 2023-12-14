c998ccf386ea7588678bf6d187c797e83c5b0a85
David Oke <davidoke@princeton.edu>
2023-12-14 13:04:47 -0500

    add scaffolding

--- cache.py
+++ cache.py
@@ -1,1 +1,28 @@
 # Cache with cache eviction algorithm
+
+'''
+Cache works as follows:
+
+10 players per game
+
+15 players in matchmaking cache, pick top 10 to matchmake
+
+leave 10 players in the cache
+
+'''
+class Cache:
+    
+    def __init__(self):
+        pass
+    
+    def add(self):
+        pass
+    
+    def get(self):
+        pass
+    
+    def evict(self):
+        pass
+
+    def stats(self):
+        pass
--- player.py
+++ player.py
@@ -1,1 +1,26 @@
 # Player representation
+
+'''
+Players have the following attributes:
+    - overall play frequency
+    - most recent time played
+    - latency to central server
+    - number of community infractions
+    - skill level
+    - player role?
+
+All of the above attributes will contribute to a player 'score' 
+based on the matchmaking pool they are potentially added to.
+
+Add player to matchmaking cache:
+
+If cache is full:
+    - evict player with lowest score in the cache
+'''
+class Player:
+    
+    def __init__(self):
+        pass
+    
+    def score(self):
+        pass
